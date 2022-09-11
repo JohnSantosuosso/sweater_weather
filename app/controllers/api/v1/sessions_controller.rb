f69e2b4fd1995966cf1321ba8dfd2c3ed6e1cbe6
John Santosuosso <john.santosuosso1@gmail.com>
2022-09-11 18:31:44 -0400

    add sad path testing for session info

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render json: UserSerializer.start_session(user), status: 200
+    else
+      render json: { errors: "Invalid email or password" }, status: 401
+    end
+  end
+
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -12,5 +12,18 @@ class UserSerializer
         }
       }
     end
+
+    def start_session(user)
+      {
+        data: {
+          type: 'users',
+          id: user.id,
+          attributes: {
+            email: user.email,
+            api_key: user.auth_token
+          }
+        }
+      }
+    end
   end
 end
