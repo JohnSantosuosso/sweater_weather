ea30433f1247a98f3b93234ad362bf51b761c28d
John Santosuosso <john.santosuosso1@gmail.com>
2022-09-10 21:40:43 -0400

    create initial route to usercontroler

---
+++ spec/requests/api/v1/user_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "User Registation", type: :request do
+  describe 'expose user registration endpoint, happy path' do
+    it 'returns a 201 status code' do
+      headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+      user_data = { "email": "john@gmail.com", "password": "password123", "password_confirmation": "password123" }
+      post '/api/v1/users', headers: headers, params: JSON.generate(user_data)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+    end
+  end
+end
