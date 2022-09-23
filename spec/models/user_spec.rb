5576d9ca98440aa8fd660e0052aa1277b555823e
John Santosuosso <john.santosuosso1@gmail.com>
2022-09-23 14:42:58 -0400

    add model testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,11 @@ describe User, type: :model do
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
   end
+
+  describe "class methods" do
+    it "can generate an auth token" do
+      user = User.create(email: "John@gmail.com", password: "password", password_confirmation: "password")
+      expect(user.auth_token).to_not be_nil
+    end
+  end
 end
