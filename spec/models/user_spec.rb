326a1da3c2587b9909861a90bca33faa856a080b
John Santosuosso <john.santosuosso1@gmail.com>
2022-09-23 14:43:52 -0400

    add additional model testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,8 @@ describe User, type: :model do
     it "can generate an auth token" do
       user = User.create(email: "John@gmail.com", password: "password", password_confirmation: "password")
       expect(user.auth_token).to_not be_nil
+      expect(user.auth_token).to be_a(String)
+      expect(user.auth_token.length).to eq(24)
     end
   end
 end
