615cf2a86736fe90a362c872fd7b19e55b024b47
John Santosuosso <john.santosuosso1@gmail.com>
2022-09-26 12:20:33 -0400

    run postman test

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe BookFacade do
     expect(books).to be_a(Hash)
     expect(books[:numFound]).to be_a(Integer)
     expect(books[:docs].count).to eq(5)
-    expect(books[:docs].first).to have_key(:isbn)
-    expect(books[:docs].first[:isbn]).to be_a(Array)
-    expect(books[:docs].first).to have_key(:title)
-    expect(books[:docs].first[:title]).to be_a(String)
-    expect(books[:docs].first).to have_key(:publisher)
-    expect(books[:docs].first[:publisher]).to be_a(Array)
+    expect(books[:docs].last).to have_key(:isbn)
+    expect(books[:docs].last[:isbn]).to be_a(Array)
+    expect(books[:docs].last).to have_key(:title)
+    expect(books[:docs].last[:title]).to be_a(String)
+    expect(books[:docs].last).to have_key(:publisher)
+    expect(books[:docs].last[:publisher]).to be_a(Array)
   end
 end
